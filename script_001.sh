72a6be1797cd3a1cdf1c3fc6ab0595add409a912
Pashibo <polichkaaa@mail.ru>
2021-09-10 23:37:25 +0400

    one

---
+++ script_001.sh
@@ -0,0 +1,9 @@
+cd /g/GitFile/QAVadim/group_22
+mkdir abc1 abc2 abc3
+cd /g/GitFile/QAVadim/group_22/abc3
+echo >> "abc3_1.txt" >> "abc3_2.txt" >> "abc3_3.txt" >> "abc3_4.json" >> "abc3_5.json"
+cd /g/GitFile/QAVadim/group_22/abc3
+mkdir abc3_1 abc3_2 abc3_3
+ls -la
+mv "abc3_1.txt" "abc3_4.json" /g/GitFile/QAVadim/group_22/abc2
+
